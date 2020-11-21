e6f15d8198860ad4001a039f62113ba132f311d1
GindaChen <32371474+GindaChen@users.noreply.github.com>
2020-11-20 23:07:37 -0600

    Add a minimally successful hadoop setup
    Accompanied with a myhadoop.tar n somewhere else

---
+++ minimal-hadoop.sh
@@ -0,0 +1,13 @@
+# =========================
+# Bashrc Settings
+# =========================
+# export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre
+# export HADOOP_HOME=/root/hadoop-3.1.4
+# export PATH=$HOME/hadoop-3.1.4/sbin:$HOME/hadoop-3.1.4/bin:$PATH
+# export LD_LIBRARY_PATH=/root/hadoop-3.1.4/lib/native
+
+# The minimal hadoop configuration:
+#   - This is just for convenience. 
+#     Later we will construct a script to start this much more wisely...
+# https://drive.google.com/file/d/1fICGjZo2YNvIDbgbHXm4RuYPpO4mVEn1/view?usp=sharing
+
